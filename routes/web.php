097fcad8332b3fb96cedebe5238dba4bc5124a5c
JacobViertel <jacobviertel@gmx.de>
2022-02-28 13:32:50 +0100

    jetstream authi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,14 @@ use App\Http\Controllers\ContactController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/home', function () {
-    echo "This is the home page";
-});
-
-Route::get('/about', function () {
-    return view('about');
-} );
-
-Route::get('/contact', [ContactController::class, 'index'])->name('con');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
